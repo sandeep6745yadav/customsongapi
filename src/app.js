dcbefabbe748ea660d44200cc0fd6317dbce0d00
sandeep6745yadav <sysandeep103@gmail.com>
2024-02-27 16:35:00 +0530

    songapi

--- src/app.js
+++ src/app.js
@@ -1,27 +1,32 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require("express");
 require("../src/db/conection");
 const song =require ("../src/models/song");
 const app = express();
 
-const port = process.env.port || 3000;
+const PORT = process.env.port || 9000;
 app.use(express.json());
  
 app.get("/song",async(req,res) => {
-    res.send("hello sandeep");
+    try{
+    const getsongs = await song.find({});
+    res.send(getsongs);
+    }catch(e){
+    res.status(400).send(e);
+    }
 });
 
 app.post("/song",async(req,res) =>{ 
     try{
         const addingSongRecords = new song(req.body) 
         console.log(req.body);
-            const insertSong = await addingSongRecords.save();
+            const insertSong = await addingSongRecords.save(); 
         
         res.status(201).send(insertSong);
     } catch(e) {
         res.status(400).send(e);
     }
 })
-app.listen(port, () => {
-    console.log(`connection is live at port no. ${port}`);
+app.listen(PORT, () => {
+    console.log(`connection is live at port no. ${PORT}`);
 });
